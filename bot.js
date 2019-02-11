bd06c4830c25d2c3a36f640141d01428855f133b
Andrew S <andrews54757@gmail.com>
2019-02-11 00:26:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,31 +7,33 @@ const settings = require('./settings.json');
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
-  console.log('I am ready to make your server a better place!');
+    console.log('I am ready to make your server a better place!');
 });
 
 bot.on('message', message => {
-  if (
-    message.author != bot.user &&
-    message.channel.type != 'dm' &&
-    Punishment.checkProfanity(message.content)
-  ) {
-    message.delete().then(msg => {
-      Punishment.doleOutPunishment(bot, msg.member, msg.guild);
-    });
-  }
+    if (
+        message.author != bot.user &&
+        message.channel.type != 'dm'
+    ) {
+        var word = Punishment.checkProfanity(message.content);
+        if (word) {
+            message.delete().then(msg => {
+                Punishment.doleOutPunishment(bot, msg.member, msg.guild, word);
+            });
+        }
+    }
 });
 
 bot.on('messageDelete', message => {
-  Logging.logMessageDelete(message);
+    Logging.logMessageDelete(message);
 });
 
 bot.on('messageUpdate', (oldMessage, newMessage) => {
-  Logging.logMessageUpdate(oldMessage, newMessage);
+    Logging.logMessageUpdate(oldMessage, newMessage);
 });
 
 bot.on('guildBanAdd', (guild, user) => {
-  Logging.logUserBan(user, guild);
+    Logging.logUserBan(user, guild);
 });
 
 bot.login(settings['BOT_LOGIN_TOKEN']);
