d11ea7351b110a60113214f668e06a06a1cb93ab
Andrew S <andrews54757@gmail.com>
2019-02-11 00:25:24 -0500

    Update punishment.js

--- punishment.js
+++ punishment.js
@@ -4,16 +4,16 @@ var bannedUsers = {};
 const MAX_WARNINGS = 1;
 
 module.exports = {
-  doleOutPunishment: function(bot, member, guild) {
+  doleOutPunishment: function(bot, member, guild, word) {
     if (!bannedUsers[guild.id]) {
       bannedUsers[guild.id] = {};
     }
     var warnings = bannedUsers[guild.id][member.id];
     if (!warnings) {
-      this.warnUser(bot, member);
+      this.warnUser(bot, member,word);
       bannedUsers[guild.id][member.id] = 1;
     } else if (warnings < MAX_WARNINGS) {
-      this.warnUser(bot, member);
+      this.warnUser(bot, member,word);
       bannedUsers[guild.id][member.id]++;
     } else if (warnings == MAX_WARNINGS) {
       this.banUser(member);
@@ -26,14 +26,13 @@ module.exports = {
     member.ban(7).catch(console.error);
   },
 
-  warnUser: function(bot, member) {
-    bot.fetchUser(member.id).then(user => user.send('Your message has been deleted for profanity and logged.'));
+  warnUser: function(bot, member,word) {
+    bot.fetchUser(member.id).then(user => user.send('Your message has been deleted for profanity and logged. You said "' + word.original + '"' + (word.original == word.word ? "" : (' which sounds like "' + word.word + '"'))));
   },
 
   checkProfanity: function(message) {
     var result = NoSwearing(message);
-    return result.every((word)=>{
-      return word.info == 2;
-    });
+    for (var i = 0; i < result.length; i++)
+      if (result[i].info == 2) return result;
   },
 };
